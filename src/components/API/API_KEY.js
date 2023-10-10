c9dfdbb8840f51d539d5ba84efd55ef21d70a75e
Вікторія <viktoriya23071@gmail.com>
2023-10-10 10:33:12 +0300

    API key

---
+++ src/components/API/API_KEY.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+const API_KEY = 'dbdaf7f3b1704351fa5cae8bb683f93c';
+
+export async function getMovieTrends(currentPage) {
+  try {
+
+    const { data } = await axios.get(`trending/movie/day?api_key=${API_KEY}&page=${currentPage}`);
+
+
+    return data.results;
+
+  } catch (error) {
+    return Notiflix.Notify.failure(
+      `${error.message}, please contact the administrator`
+    );
+  }
+}
+
+export async function getMovieByQuery (query) {
+
+
+  try {
+
+    const {data} = await axios.get(`search/movie?api_key=${API_KEY}&query=${query}`);
+
+    return data.results;
+  } catch (error) {
+
+    return Notiflix.Notify.failure(
+      `${error.message}, plese contact the administrator`
+    )
+  }
+}
+
+export async function getMovieById(id) {
+  try {
+    const { data } = await axios.get(`movie/${id}?api_key=${API_KEY}`);
+
+    return data;
+  } catch (error) {
+    return Notiflix.Notify.failure(
+      `${error.message}, please contact the administrator`
+    );
+  }
+}
+
+export async function getCastById(id) {
+  try {
+    const { data } = await axios.get(
+      `movie/${id}/credits?api_key=${API_KEY}`
+    );
+
+    return data;
+  } catch (error) {
+    return Notiflix.Notify.failure(
+      `${error.message}, please contact the administrator`
+    );
+  }
+}
+
+
+export async function getReviewsById (id) {
+
+    try {
+
+        const {data} = await axios.get(
+            `movie/${id}/reviews?api_key=${API_KEY}`
+        )
+
+        return data;
+    } catch(error) {
+        return Notiflix.Notify.failure(
+            `${error.message}, please contact the administrator`
+          );
+    }
+}
