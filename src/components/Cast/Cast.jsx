5587b75b7f598a755fc4bf33e21ea6242e44dcf4
Вікторія <viktoriya23071@gmail.com>
2023-10-12 17:36:08 +0300

    placeholder

---
+++ src/components/Cast/Cast.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { getCastById } from 'API/API_KEY';
+import { useParams } from 'react-router-dom';
+import { List, Item, Title, SubTitle } from './Cast.styled';
+import Pla
+
+const Cast = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Cast
