40c547617a8f58fbc627d44d943c3faf831e11f5
Вікторія <viktoriya23071@gmail.com>
2023-10-12 17:45:46 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,16 +1,1 @@
-import { useEffect, useState } from 'react';
-import { getCastById } from 'API/API_KEY';
-import { useParams } from 'react-router-dom';
 
-import Placeholder from '../Placeholder/Placeholder';
-
-
-const Cast = () => {
-  return (
-    <div>
-
-    </div>
-  )
-}
-
-export default Cast
