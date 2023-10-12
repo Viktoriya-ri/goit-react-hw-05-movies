5d35da376713639adc0b28862f7e052a5ce1062d
Вікторія <viktoriya23071@gmail.com>
2023-10-12 17:39:14 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react';
 import { getCastById } from 'API/API_KEY';
 import { useParams } from 'react-router-dom';
 import { List, Item, Title, SubTitle } from './Cast.styled';
-import Pla
+import Placeholder from '../Placeholder/Placeholder';
+
 
 const Cast = () => {
   return (
