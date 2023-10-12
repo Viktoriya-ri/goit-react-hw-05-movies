d42c4329da1e913dffed90051a8575003174b42c
Вікторія <viktoriya23071@gmail.com>
2023-10-12 22:22:35 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,24 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header, Nav, StyledNavLink } from './SharedLayout.styled';
+import { FcHome, FcFilmReel } from 'react-icons/fc';
+import Loader from 'components/Loader/Loader';
 
 const SharedLayout = () => {
   return (
     <div>
+      <Header>
+        <Nav>
+          <StyledNavLink to='/'><FcHome size='22' />Home</StyledNavLink>
+          <StyledNavLink to='/movies'><FcFilmReel size='22' />Movies</StyledNavLink>
+        </Nav>
+      </Header>
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
 
     </div>
-  )
-}
+  );
+};
 
-export default SharedLayout
+export default SharedLayout;
