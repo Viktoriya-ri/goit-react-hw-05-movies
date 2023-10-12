bdb397b4ea164a371242649683932f4f650c84a0
Вікторія <viktoriya23071@gmail.com>
2023-10-12 23:20:08 +0300

    movies

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
+import { useState, useEffect, useRef } from 'react';
+import { getMovieById } from 'API/API_KEY';
+import MovieCard from 'components/MovieCard/MovieCard';
+import Loader from 'components/Loader/Loader';
+import Error from 'components/Error/Error';
+import { Notify } from 'notiflix';
+import { NavLink, useParams, Outlet, useLocation, Link } from 'react-router-dom';
+import { Title, List, Item } from './MovieDetails.styled';
 
 const MovieDetails = () => {
+  const location = useLocation();
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [failure, setFailure] = useState(false);
+  const backLinkHref = useRef(location.state?.from ?? '/');
+
+  useEffect(() => {
+
+    const fetchMovie = async () => {
+      try {
+        setLoader(true);
+        const movieData = await getMovieById(movieId);
+        setMovie(movieData);
+      } catch (error) {
+        setFailure(true);
+        Notify.failure(error.message);
+      } finally {
+        setLoader(false);
+      }
+    };
+    fetchMovie();
+
+  }, [movieId]);
+
+  const { title, vote_average, overview, genres, poster_path } = movie;
+  const genreNames = genres ? genres.map(genre => genre.name).join('') : '';
+  const posterPath = poster_path ? poster_path : '';
+
   return (
-    <div>
+    <>
+      <Link to={backLinkHref.current}>Back to movies</Link>
+      <MovieCard
+        posterPath={posterPath}
+        title={title}
+        vote_average={overview}
+        genreNames={genreNames}
+      />
+      {loader && <Loader />}
+      {failure && <Error />}
+      <Title>Additional informations</Title>
+      <List>
+        <Item>
+          <NavLink to={`cast`}>Cast</NavLink>
+        </Item>
+        <Item>
+          <NavLink to={`reviews`}>Reviews</NavLink>
+        </Item>
+      </List>
+      <Outlet />
+    </>
+  );
+};
 
-    </div>
-  )
-}
-
-export default MovieDetails
+export default MovieDetails;
 
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Notify } from "notiflix";
+import { getMovieByQuery } from "API/API_KEY";
+import MoviesList from "components/MoviesList/MoviesList";
+import Form from "components/Form/Form";
+import { useSearchParams } from "react-router-dom";
+import Loader from "components/Loader/Loader";
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [failure, setFailure] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const currentParamsQuery = searchParams.get('query');
+
+    if (!currentParamsQuery) return;
+
+    const fetchedMovies = async () => {
+      try {
+        setLoader(true);
+        const fetchedData = await getMovieByQuery(currentParamsQuery);
+
+        if (fetchedData.length === 0) {
+          Notify.failure("We've found nothig by this query");
+        }
+        setMovies(fetchedData);
+      } catch (error) {
+        setFailure(false);
+        Notify.failure(error.message);
+      } finally {
+        setLoader(false);
+      }
+    };
+    fetchedMovies();
+  }, [searchParams]);
+
   return (
-    <div>
+    <>
+      <Form setSearchParams={setSearchParams} />
+      {movies && movies.length > 0 && <MoviesList movies={movies} />}
+      {loader && <Loader />}
+      {failure &&
+        <strong>Something went wrong, please contact the administrator</strong>
+      }
+    </>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Movies
+export default Movies;
