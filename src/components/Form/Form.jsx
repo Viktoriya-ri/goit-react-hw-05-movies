48b45813e311cb5f6a45871c48f2b590235093f0
Вікторія <viktoriya23071@gmail.com>
2023-10-12 19:20:53 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,2 +1,31 @@
+import { useState } from 'react';
+import { Notify } from 'notiflix';
+
+const Form = ({ setSearchParams }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = e => {
+    setQuery(e.target.value);
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (query.trim() === '') {
+      return Notify.failure('query can`t be empty');
+    }
+    setSearchParams({ query });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+
+      <input value={query} autoFocus onChange={e => handleChange(e)} name='text' />
+      <button type='submit'>Search</button>
+
+    </form>
+  )
+}
+
+export default Form;
 
 
